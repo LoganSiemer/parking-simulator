2e34601b4d6e77cfebfac67acf72650648fae043
Logan Siemer <logansiemer@Logans-MacBook.attlocal.net>
2018-01-03 17:38:18 -0600

    i dont know

--- index.js
+++ index.js
@@ -7,56 +7,89 @@
 var cars = 100;
 var i = 0;
 
-function makeCar(color, model, licensplate, year){
-	var car = {
-		color : color,
-		model : model,
-		licenseplate : licensplate,
-		year : year
-	};
-	return car;
+function Car(color, model, licensplate, year){
+	this.color = color;
+	this.model = model;
+	this.licenseplate = licensplate;
+	this.year = year;
 }
-console.log(makeCar("blue","ford","1","2017"));
+console.log(new Car("blue","ford","1","2017"));
+
 function buildCars(count){
     var result = [];
+
     for (var i = 0; i < count; i++) {
-        var car = makeCar("blue","ford",i,"2017");
+        var car = new Car("blue","ford",i,"2017");
+        var parked = false;
         result.push(car);
     }
     return result;
 }
+var unparkedCars = buildCars(100);
 
-console.log(buildCars(101));
+var parkedCars = [];
 
+var unusedPlots = [];
 
+var usedPlots = [];
 
-
-var plots = 10;
-var plot = [' '];
-var i = 0;
-
-do {
-    console.log(i + 1);
-
-    i++;
-} while (i < plots){
-
+function Plot(number){
+    this.number = number;
 }
-var is_car_parked = false;
+function buildPlots(){
+	for (var i = 0; i < 10; i++) {
+		unusedPlots.push(new Plot(i));
+	}
+}
+buildPlots();
 
-function park(){
-	is_car_parked = true;
+
+
+function parkOneCar(){
+	if(unparkedCars.length > 0 && unusedPlots.length > 0){
+		var car = unparkedCars.splice(0, 1);
+		var plot = unusedPlots.splice(0, 1);
+		parkedCars.push(car[0]);
+		usedPlots.push(plot[0]);
+
+		console.log("(" + Car.color + ")" + " - " + "(" + Car.model + ")" + " - " + "(" + Car.licenseplate + ")" + " - "+ "(" + Car.year + ")");
+
+
+		ScheduleCarToLeave(car[0], plot[0]);
+    }
 }
 
-var is_car_removed = false;
+function ScheduleCarToLeave(car, plot){
+     setTimeout(function(){
+        var carIndex = parkedCars.indexOf(car);
+        parkedCars.splice(carIndex, 1);
 
-function remove(){
-	is_car_removed = true;
+        var plotIndex = usedPlots.indexOf(plot);
+        usedPlots.splice(plotIndex, 1);
+
+        unusedPlots.push(plot);
+
+        console.log("car has left the building");
+     }, 1000);
 }
 
+var intervalId = setInterval(function(){
 
+    parkOneCar();
 
+    if (unparkedCars.length == 0) {
+    	clearInterval(intervalId);
+    }
 
+}, 1000);
+
+console.log(unparkedCars);
+
+console.log(parkedCars);
+
+console.log(unusedPlots);
+
+console.log(usedPlots);
 
 
 
